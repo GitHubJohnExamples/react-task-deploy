c5e6bb71c3a7107b27c99481213b768650579ce1
jona <jona5917@gmail.com>
2023-05-30 00:09:31 +0100

    first deploy

---
+++ src/context/TaskContext.jsx
@@ -0,0 +1,44 @@
+//context from react
+import { createContext, useEffect, useState } from 'react'
+import { tasks as data } from "../data/tasks";
+
+export const TaskContext = createContext()
+
+//provider
+export function TaskContextProvider(props) {
+
+    const [tasks, setTasks] = useState([]);
+
+    function createTask(task) {
+        setTasks([...tasks, {
+            title: task.title,
+            id: tasks.length,
+            description: task.description
+        }])
+
+    }
+
+    function deleteTask(taskId) {
+        setTasks(tasks.filter(task => task.id !== taskId))
+    }
+
+    useEffect(() => {
+        setTasks(data);
+    }, []);
+
+    return (
+        <TaskContext.Provider
+            value={{
+                // can be:
+                //tasks,
+                //deleteTask,
+                //createTask
+
+                tasks: tasks,
+                deleteTask: deleteTask,
+                createTask: createTask
+            }}>
+            {props.children}
+        </TaskContext.Provider>
+    )
+}
